13155c45b36669e294b116caba05050283a96443
FranTedesco <tedesco.uni@gmail.com>
2019-02-16 18:03:51 +0100

    modifica del parserMain.py per farlo funzionare senza che scorra tutte le cartelle copiato VerificaCaricamentoDinamico.py solo per comodità in modo da poterlo importare in parserMain.py senza dover scrivere il percorso completo

---
+++ parser/Parser main/VerificaCaricamentoDinamico.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import os.path
+import subprocess
+
+##Comando per fare il grep nei file java:
+##  find -name '*.java' -exec grep --color=always -in "ClassLoader" {} 2>/dev/null +
+
+#definizione delle varie parti della stringa del comando
+parteIniziale='find -name "*.java" -exec grep -in '
+parteFinale=' {} 2>/dev/null +'
+stringhe=[]
+#definizione degli indicatori di caricamento dinamico
+stringhe.append('"dalvik.system.DexClassLoader"')
+stringhe.append('"dalvik.system.PathClassLoader"')
+stringhe.append('"dalvik.system.InMemoryDexClassLoader"')
+stringhe.append('"dalvik.system.BaseDexClassLoader"')
+stringhe.append('"dalvik.system.DelegateLastClassLoader"')
+stringhe.append('"java.net.URLClassLoader"')
+stringhe.append('"java.lang.reflect"')
+
+#definizione del metodo di verifica sulla singola applicazione
+def verificaIndicatori(listaIndicatori):
+	CaricamentoDinamico = False
+	for i in stringhe:
+		comando = parteIniziale+i+parteFinale
+		proc = subprocess.Popen([comando],shell=True, stdout=subprocess.PIPE,)
+		stdout_value=proc.communicate()[0]
+		if stdout_value:
+			CaricamentoDinamico= True
+			listaIndicatori.append(stdout_value)
+	return CaricamentoDinamico
+
+
+#la funzione prevede i due percorsi contenenti le due versioni delle app come input.
+#la funzione restituisce la lista delle differenze tra i due caricamenti dinamici se ci sono. Altrimenti restituisce una lista vuota se entrambe le app presentano gli stessi identici indicatori di caricamento dinamico. Restituisce infine un oggetto None se non ci sono indicatori di caricamento dinamico in nessuna delle due app
+def CaricamentoDinamico(percorso1,percorso2):
+	#variabili di supporto
+	#variabili che mi rappresentano gli indicatori di caricamento dinamico nelle due app
+	Caricamento1 = False
+	Caricamento2 = False
+	#lista degli indicatori di caricamento dinamico delle due app
+	listaIndicatori1=[]
+	listaIndicatori2=[]
+	#mi sposto nella cartella contenente la prima applicazione 
+	os.chdir(percorso1)
+
+	#verifico la presenza del caricamento dinamico nella prima app
+	CaricamentoDinamico1 = verificaIndicatori(listaIndicatori1)
+
+	#mi sposto nella cartella contenente la seconda applicazione 
+	os.chdir(percorso2)
+
+	#verifico la presenza del caricamento dimanico nella seconda app
+	CaricamentoDinamico2 = verificaIndicatori(listaIndicatori2)
+
+	#faccio la differenza tra i due set
+	if(CaricamentoDinamico1 or CaricamentoDinamico2):
+		lista=list(set(listaIndicatori1) - set(listaIndicatori2))
+		lista=lista+list(set(listaIndicatori2) - set(listaIndicatori1))
+		return lista
+	else:
+		return None
+
+#linea utilizzata per verificare che il programma funzioni
+#print(CaricamentoDinamico("/home/lucio/Scrivania/Progetto Sicurezza/ApkDec2400/9039/oad1fc0d7d0","/home/lucio/Scrivania/Progetto Sicurezza/ApkDec2400/9039/rfd5b5c7dac"))
--- parser/Parser main/parserMain.py
+++ parser/Parser main/parserMain.py
@@ -18,14 +18,36 @@ dir = "D:\Apk_Dec1-1199"
 secondo = False # serve per vedere se ho preso il secondo file in modo da poter fare la differenza
 
 
+for item in os.listdir(dir): #cicla per ogni elemento di quella cartella, senza vedere le eventuali sotto cartelle
+    itemfullpath = os.path.join(dir, item) #ottengo percorso completo
+    print(itemfullpath)
+    for i in os.listdir(itemfullpath): #altra sottocartella
+        ifullpath = os.path.join(itemfullpath, i)
+        print(ifullpath)
+
+        filejar= []
+        if i.endswith(".jar"): #salvo il percorso dei file jar che trovo in un array, i primi due (dovrebbero sempre essere solo due) li passo come parametro per il confronto delle dipendenze
+            print("fle jar")
+
+        '''
+        if os.path.isdir(os.path.join(itemfullpath, i)): #serve per controllare se l'elemento è una cartella
+            print()
+        '''
+
+
+
+
+
+'''
 #Esplora tutte le directory a partire dal path specificato (walk)
 for root, dirs, files in os.walk(dir, topdown=False):
 
+
+ 
     # cerca tutti i file e stampa nel caso trova i file chiamatai AndroidManifest.xml
     for f in files:
         if f.endswith("AndroidManifest.xml"):
             path = os.path.join(root, f) #ottengo il path completo del file trovato
-
             # PATCH DA DISCUTERE, il problema è che esiste un file xml nella cartella original che però non è ben formattato e che quindi da errore
             if "original" not in path:
                 if secondo == False:
@@ -46,5 +68,5 @@ for root, dirs, files in os.walk(dir, topdown=False):
                     print()
                     secondo = False
                # print(*permessi,sep="\n")  # stampa tutta la lista senza parentesi e dividendo ogni oggetto della lista con il valore sep
-
+    '''
 
